02d3cbd893b834326f89171b90423ddcdbf3b0ce
Mikkobelly <mikiakuta0529@gmail.com>
2023-01-20 02:06:23 +0000

    made pokemonRepository as IIFE and updated for loop to forEach()

--- js/script.js
+++ js/script.js
@@ -1,32 +1,59 @@
-let pokemonList = [];
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: 'Bulbasaur',
+            height: 7,
+            types: ['grass', 'poison']
+        },
+        {
+            name: 'Charmander',
+            height: 6,
+            type: ['fire']
+        },
+        {
+            name: 'Squirtle',
+            height: 5,
+            type: ['water']
+        },
+        {
+            name: 'Caterpie',
+            height: 3,
+            type: ['bug']
+        }
+    ];
 
-pokemonList = [
-    {
-        name: 'Bulbasaur',
-        height: 7,
-        types: ['grass', 'poison']
-    },
-    {
-        name: 'Charmander',
-        height: 6,
-        type: ['fire']
-    },
-    {
-        name: 'Squirtle',
-        height: 5,
-        type: ['water']
-    },
-    {
-        name: 'Caterpie',
-        height: 3,
-        type: ['bug']
+    function getAll() {
+        return pokemonList;
     }
-];
 
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height >= 7) {
-        document.write(`<p>${i + 1}. ${pokemonList[i].name} (height: 0.${pokemonList[i].height}m) - Wow, that' big!</p>`)
+    function add(item) {
+        pokemonList.push(item);
+    }
+
+    function addV(item) {
+        if (typeof item === 'object' && Object.keys(item) === ['name', 'height', 'type']) {
+            pokemonList.push(item);
+        } else {
+            return 'Not valid data type';
+        }
+    }
+
+    function findPokemon(pokemonName) {
+        pokemonList.filter(function (pokemon) {
+            return pokemon.name === pokemonName;
+        })
+    }
+
+    return { getAll, add, addV, findPokemon };
+})();
+
+
+let allPokemons = pokemonRepository.getAll();
+
+allPokemons.forEach(function (item, index) {
+    if (item.height >= 7) {
+        document.write(`<p>${index + 1}. ${item.name} (height: 0.${item.height}m) - Wow, that' big!</p>`)
     } else {
-        document.write(`<p>${i + 1}. ${pokemonList[i].name} (height: 0.${pokemonList[i].height}m)`)
+        document.write(`<p>${index + 1}. ${item.name} (height: 0.${item.height}m)</p>`)
     }
-}
+})
